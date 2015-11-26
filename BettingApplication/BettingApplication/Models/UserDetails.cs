a472e1ba9c8399a269f963e7455a0210839c0c7b
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-11-26 15:25:54 +0100

    Created user class

---
+++ BettingApplication/BettingApplication/Models/UserDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BettingApplication.Models
+{
+  public class UserDetails
+  {
+    public List<Fixtures> UserList { get; set; }
+    public int Id { get; set; }
+    public string UserEmail { get; set; }
+    public string PlacedBets { get; set; }
+  }
+}
