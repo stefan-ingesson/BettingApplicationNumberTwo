8b22431a96a440f7eefd2bb66249be3a048f0a37
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-11-27 11:30:06 +0100

    Kan välja omgång och se resultaten

---
+++ BettingApplication/BettingApplication/Models/FixtureViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BettingApplication.Models
+{
+    public class FixtureViewModel
+    {
+        public IEnumerable<Fixtures.Fixture> fixtures { get; set; }
+
+        public IEnumerable<SelectListItem> matchDays { get; set; }
+
+        public List<Fixtures.Fixture> status {get; set;}
+
+        public List<Fixtures.HomeTeam> homeTeam { get; set; }
+
+        public List<Fixtures.AwayTeam> awayTeam { get; set; }
+
+        public List<Fixtures.Result> goalsHomeTeam { get; set; }
+
+        public List<Fixtures.Result> goalsAwayTeam { get; set; }
+
+
+
+        //public List<SelectListItem> GetMatchDayList(int? matchday)
+        //{
+
+        //    var query = fixtures.OrderByDescending(x => x.matchday).ToList()
+        //       .Select(x => new SelectListItem
+        //       {
+        //           Value = x.matchday.ToString(),
+        //           Text = x.matchday.ToString()
+        //       });
+
+        //    return (query).ToList();
+
+        //}
+
+
+       
+
+        //public int? matchDay { get; set; }
+    }
+}
