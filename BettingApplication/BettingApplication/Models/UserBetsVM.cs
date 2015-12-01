bb5014b0ef3870f81430b2321005234a713e37a5
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-12-01 12:26:49 +0100

    Mysko

---
+++ BettingApplication/BettingApplication/Models/UserBetsVM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace BettingApplication.Models
+{
+
+  public class UserBetsVM
+  {
+    public Bets Bets { get; set; }
+
+    public IEnumerable<Fixtures.Fixture> status { get; set; }
+
+    public Fixtures.Fixture date { get; set; }
+
+    public Fixtures.Fixture HomeTeamWins { get; set; }
+
+    public Fixtures.Fixture AwayTeamWins { get; set; }
+
+    public Fixtures.Fixture Draw { get; set; }
+
+    public Fixtures.HomeTeam homeTeam { get; set; }
+
+    public Fixtures.AwayTeam awayTeam { get; set; }
+
+    public Fixtures.Result goalsHomeTeam { get; set; }
+
+    public Fixtures.Result goalsAwayTeam { get; set; }
+
+  }
+}
